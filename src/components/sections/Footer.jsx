4fd71f8fab4a08b2c0e9116e69346361f88f5b6f
jonathan <68912218+jonathan-conforme@users.noreply.github.com>
2026-04-28 23:44:56 -0500

    fix rutas imagenes build limpio

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -11,6 +11,7 @@ import {
     Camera,
     Globe
 } from "lucide-react"
+
 import guayaquil from "@/assets/logo-guayaquil.png"
 import pichincha from "@/assets/logo-pichincha.png" 
 
@@ -154,7 +155,7 @@ export default function Footer() {
                                 {/* Logo Banco Pichincha */}
                                 <div className="bg-white p-1.5 rounded-lg border border-gray-100 shadow-sm flex items-center justify-center h-10 w-24">
                                     <img
-                                        src="/img/logo-pichincha.png" /* IMPORTANTE: Reemplaza con tu ruta real */
+                                        src={pichincha} /* IMPORTANTE: Reemplaza con tu ruta real */
                                         alt="Banco Pichincha"
                                         className="h-full w-auto object-contain" /* Mantiene proporción y centra */
                                     />
